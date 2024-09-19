3452b8df74c7047a90000a74d9fa33a82ca2cc63
Ariel Araya-Madrigal <58918901+Ariel100Araya@users.noreply.github.com>
2024-09-19 12:38:24 -0400

    Add files via upload

---
+++ Week One/MyFirstProgram.java
@@ -0,0 +1,8 @@
+
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        String FirstName = "Ariel";
+        String LastName = "Araya";
+        System.out.println("Hello " + FirstName + " " + LastName + "!");
+    }
+}
