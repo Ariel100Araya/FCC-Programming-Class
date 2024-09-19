a69c5524fb4920ddca65ef0091085849d10c14eb
Ariel Araya-Madrigal <58918901+Ariel100Araya@users.noreply.github.com>
2024-09-19 12:38:37 -0400

    Delete MyFirstProgram.java

--- MyFirstProgram.java
+++
@@ -1,8 +0,0 @@
-
-public class MyFirstProgram {
-    public static void main(String[] args) {
-        String FirstName = "Ariel";
-        String LastName = "Araya";
-        System.out.println("Hello " + FirstName + " " + LastName + "!");
-    }
-}
